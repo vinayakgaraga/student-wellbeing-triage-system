b7d151f77630bc3085b0ca0980a329f2700d0048
vinayakgaraga <vinayakgaraga@gmail.com>
2026-07-12 23:08:01 +0530

    Add email-validator dependency

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,7 +36,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://*.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
